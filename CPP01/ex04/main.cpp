5a7d371d84041813ac15f28d0999cc78915309ee
Kero <matias.quero@gmail.com>
2025-03-31 19:52:07 +0300

    das

--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mquero <mquero@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 11:49:45 by mquero            #+#    #+#             */
-/*   Updated: 2025/03/18 12:40:42 by mquero           ###   ########.fr       */
+/*   Updated: 2025/03/31 19:51:52 by mquero           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,36 +14,42 @@
 #include <fstream>
 #include <string>
 
+
 int main(int argc, char const *argv[])
 {
-    if(argc < 2)
-        return 0;
-    std::string myText;
-    std::ifstream MyReadFile(argv[1]);
-    std::string file1 = argv[1];
-    std::ofstream newFile(file1.append(".replace"));
-    std::string cpyString =argv[3];
-    std::string srcString =argv[2];
-    std::string finalString = "";
-    size_t pos = 0;
-    size_t prev = 0;
-
-    while (getline(MyReadFile, myText)) 
-    {
-        pos = myText.find(argv[2]);
-        prev = 0;
-        while (pos != std::string::npos)
-        {
-            finalString.append(myText, prev, prev - pos);
-            finalString.append(cpyString, pos);
-            prev = pos + srcString.length();
-            pos = myText.find(argv[2], prev);
-        }
-        finalString.append(myText, prev, myText.length() - prev);
-        finalString.append("\n");
+    if (argc != 4) {
+        std::cerr << "Wrong input " << argv[0] << std::endl;
+        return 1;
     }
-    newFile << finalString << std::endl;
-    MyReadFile.close();
-    newFile.close();
+    std::string filename = argv[1];
+    std::string s1 = argv[2];
+    std::string s2 = argv[3];
+
+    std::ifstream inputFile(filename);
+    if (!inputFile) {
+        std::cerr << "Cannot open file " << argv[0] << std::endl;
+        return 1;
+    }
+
+    std::ofstream outputFile(filename + ".replace");
+    if (!outputFile) {
+        std::cerr << "Cannot create file " << argv[0] << std::endl;
+        return 1;
+    }
+    std::string line;
+    while (std::getline(inputFile, line)) 
+    {
+        size_t pos = line.find(s1);
+        while (pos != std::string::npos) 
+        {
+            pos = line.find(s1,pos);
+            line.erase(pos, s1.length());
+            line.insert(pos, s2);
+            pos = line.find (s1, pos + s2.length()); 
+        }
+        outputFile << line << "\n";
+    }
+    inputFile.close();
+    outputFile.close();
     return 0;
 }
